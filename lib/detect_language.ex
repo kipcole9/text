893f38dc27d5be811070a991eb930149ff9c00e7
Kip Cole <kipcole9@gmail.com>
2019-08-26 13:04:16 +0400

    Basic language detection with udhr corpus

--- lib/detect_language.ex
+++ lib/detect_language.ex
@@ -1,7 +1,90 @@
 defmodule Text.DetectLanguage do
+  alias Text.Ngram
 
-  def detect_language(_text) do
+  @ngram 4
+  @no_entry :math.log(5.0e-6)
 
+  def detect_language(text, model \\ corpus_model()) do
+    text_ngrams = Ngram.ngram(text, @ngram)
+    language_candidates = Enum.map(corpus(), &(elem(&1, 0)))
+
+    language_candidates
+    |> Task.async_stream(__MODULE__, :detect_one_language, [model, text_ngrams], async_options())
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.sort_by(fn {_, {score, _, _}} -> score end)
+    |> Enum.reverse
+  end
+
+  def detect_one_language(language, model, text_ngrams) do
+    language_model = Map.get(model, language)
+
+    score =
+      text_ngrams
+      |> Enum.reduce({0, 0, 0}, fn {ngram, _count}, {acc, found, not_found} ->
+        p1 = if Map.get(language_model, ngram), do: 1, else: 0
+        p2 = if p1 == 0, do: 1, else: 0
+        probability = Map.get(language_model, ngram) || @no_entry
+        {acc + probability, found + p1, not_found + p2}
+      end)
+
+    {language, score}
+  end
+
+  def async_options do
+    [max_concurrency: System.schedulers_online() * 2, ordered: false]
+  end
+
+  def build_model do
+    model =
+      corpus()
+      |> Task.async_stream(__MODULE__, :process_corpus_entry, [], async_options())
+      |> Enum.map(&elem(&1, 1))
+      |> Map.new
+      |> :erlang.term_to_binary
+
+    File.write!(corpus_model_file(), model)
+  end
+
+  def corpus_model do
+    corpus_model_file()
+    |> File.read!
+    |> :erlang.binary_to_term
+  end
+
+  def corpus_model_file do
+    Path.join(:code.priv_dir(:text), "detect_language/udhr_ngrams.etf")
+  end
+
+  def process_corpus_entry({language, entry}, n \\ @ngram) do
+    ngrams =
+      corpus_dir()
+      |> Path.join(corpus_file(entry))
+      |> File.read!
+      |> String.split("---")
+      |> Enum.at(1)
+      |> String.trim
+      |> String.normalize(:nfc)
+      |> Ngram.ngram(n)
+      |> convert_to_probabilities()
+
+    {language, ngrams}
+  end
+
+  def convert_to_probabilities(ngrams) do
+    sum =
+      ngrams
+      |> Enum.map(&elem(&1, 1))
+      |> Enum.sum
+
+    ngrams
+    |> Enum.map(fn {ngram, count} ->
+      {ngram, :math.log(count / sum)}
+    end)
+    |> Map.new
+  end
+
+  def corpus_file(%{file: file}) do
+    "udhr/udhr_" <> file <> ".txt"
   end
 
   def corpus_dir do
