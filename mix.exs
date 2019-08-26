2eaf893bc536906cf06ccfa6a015f00966c8f1bc
Kip Cole <kipcole9@gmail.com>
2019-08-26 08:50:04 +0400

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -60,7 +60,8 @@ defmodule Text.MixProject do
   defp deps do
     [
       {:sweet_xml, "~> 0.6"},
-      {:jason, "~> 1.0"}
+      {:jason, "~> 1.0"},
+      {:ex_doc, "~> 0.21", only: [:dev, :release]}
     ]
   end
 
